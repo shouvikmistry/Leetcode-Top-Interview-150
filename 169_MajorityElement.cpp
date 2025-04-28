9c733a5036963964109d2e449975cd678093202a
Shouvik <shouvikmistry24@gmail.com>
2025-04-28 22:47:48 +0530

    Implement Majority Element algorithm

---
+++ 169_MajorityElement.cpp
@@ -0,0 +1,63 @@
+/**
+ * Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+ 
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        int majorityElement(vector<int>& nums) {
+            int maxElement = nums[0];
+            int count = 1;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (maxElement==nums[i])
+                {
+                    count++;
+                }
+                else if (count==0)
+                {
+                    maxElement = nums[i];
+                    count = 1;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+        return maxElement;
+        }
+    };
+
+int main() {
+    Solution s;
+    vector<int> nums = {3,2,3};
+     nums = {2,2,1,1,1,2,2};
+    cout << s.majorityElement(nums) << endl; 
+    return 0;
+}
